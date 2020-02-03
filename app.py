7fb5a6cdd2252cdd012e73fbc6e580609cc2c38d
shivaninethikar <60536486+shivaninethikar@users.noreply.github.com>
2020-02-03 12:35:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,51 +68,6 @@ def webhook():
                         pass
 		
 
-def tem(sender_id,message):
-  payload = {
-    "recipient":{
-    "id":"<PSID>"
-    },
-    "message":{
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Welcome!",
-            "image_url":"https://petersfancybrownhats.com/company_image.png",
-            "subtitle":"We have the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://petersfancybrownhats.com/view?item=103",
-              "webview_height_ratio": "tall",
-            },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersfancybrownhats.com",
-                "title":"View Website"
-              },{
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-              }              
-            ]      
-          }
-        ]
-      }
-    }
-  }
-}
-                   
-if messaging_text=="option":
-	payload["recipient"]["id"] = sender_id
-        bot.send_raw(payload)
-else:
-       bot.send_text_message(sender_id, "Under Maintanance)	
-    
-		
 					
                    
               
